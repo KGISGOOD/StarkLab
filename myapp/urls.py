0aa04855ae4f02a241ca2681c9a0326d61899a10
Eric <eric0909711680@gmail.com>
2024-08-08 15:28:21 +0800

    新增連結

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import add_person, person_list, index, achievements, team
+from .views import index, about, contact, gallery, products,project1,project2,project3,project4,project5,project6
 
 urlpatterns = [
     path('', index, name='index'),  # 主頁
-    path('achievements/', achievements, name='achievements'),  # 戰績頁
-    path('team/', team, name='team'),  # 人員頁
-    path('add/', add_person, name='add_person'),  # 新增人員頁
-    path('persons/', person_list, name='person_list'),  # 人員列表頁
+    path('about/', about, name='about'),  # 戰績頁
+    path('contact/', contact, name='contact'),  # 人員頁
+    path('gallery/', gallery, name='gallery'),  # 新增人員頁
+    path('products/', products, name='products'),  # 人員列表頁
+    path('project1/', project1, name='project1'),
+    path('project2/', project2, name='project2'),
+    path('project3/', project3, name='project3'),
+    path('project4/', project4, name='project4'),
+    path('project5/', project5, name='project5'),
+    path('project6/', project6, name='project6'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,11 +5,35 @@ from .models import Person
 def index(request):
     return render(request, 'index.html')
 
-def achievements(request):
-    return render(request, 'achievements.html')
+def about(request):
+    return render(request, 'about.html')
 
-def team(request):
-    return render(request, 'team.html')
+def contact(request):
+    return render(request, 'contact.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def products(request):
+    return render(request, 'products.html')
+
+def project1(request):
+    return render(request, 'project1.html')
+
+def project2(request):
+    return render(request, 'project2.html')
+
+def project3(request):
+    return render(request, 'project3.html')
+
+def project4(request):
+    return render(request, 'project4.html')
+
+def project5(request):
+    return render(request, 'project5.html')
+
+def project6(request):
+    return render(request, 'project6.html')
 
 def add_person(request):
     if request.method == "POST":
