a7d07a1f1687b93c6114175583f55ce79ba5947e
Eric <eric0909711680@gmail.com>
2024-08-23 16:21:48 +0800

    更動

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [],  # 移除 DIRS 設置，讓 Django 只使用 APP_DIRS 查找模板
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
