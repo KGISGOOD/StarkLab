44c3a91502409f2486392d5718ed669d11a37c51
Eric <eric0909711680@gmail.com>
2024-08-23 17:14:25 +0800

    Django 已上架

---
+++ LabWeb/lab/mylab/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def products(request):
+    return render(request, 'products.html')
+
+def project1(request):
+    return render(request, 'project1.html')
+
+def project2(request):
+    return render(request, 'project2.html')
+
+def project3(request):
+    return render(request, 'project3.html')
+
+def project4(request):
+    return render(request, 'project4.html')
+
+def project5(request):
+    return render(request, 'project5.html')
+
+def project6(request):
+    return render(request, 'project6.html')
+
