39d96bf7c530c96c87009c600cb9015d21f6ecc2
Ray-888 <huangxoxox@gmail.com>
2024-09-13 17:35:35 +0800

    good

--- linebottest/linetest/views.py
+++ linebottest/linetest/views.py
@@ -13,9 +13,12 @@ parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 @csrf_exempt
 def callback(request):
     if request.method == 'POST':
-        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        signature = request.META.get('HTTP_X_LINE_SIGNATURE')
         body = request.body.decode('utf-8')
-        
+
+        if not signature or not body:
+            return HttpResponseBadRequest()
+
         try:
             events = parser.parse(body, signature)
         except InvalidSignatureError:
@@ -25,8 +28,8 @@ def callback(request):
 
         for event in events:
             if isinstance(event, MessageEvent):
-                line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
-                
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
