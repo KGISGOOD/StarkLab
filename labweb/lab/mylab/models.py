e8096eb46a9c2f07d51215a06bc4a65780b0ba45
Ray-888 <huangxoxox@gmail.com>
2024-10-15 21:52:48 +0800

    model

--- labweb/lab/mylab/models.py
+++ labweb/lab/mylab/models.py
@@ -11,11 +11,11 @@ class Stock(models.Model):
 
 class News(models.Model):
     title = models.CharField(max_length=255)
+    image = models.URLField(max_length=500, blank=True, null=True)  # 新增圖片欄位
     link = models.URLField(max_length=500)
     content = models.TextField()
     source = models.CharField(max_length=100)
     date = models.CharField(max_length=100)
-
     class Meta:
         db_table = "news"
 
